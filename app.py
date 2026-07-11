f2f91252bb5bcfc4a00e6c2317ebc1152fa99a28
mkmayk11 <mkmayk11@gmail.com>
2026-07-11 13:00:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
