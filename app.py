428b2a2d9bc6524051a316019227eb1385c40224
mkmayk11 <mkmayk11@gmail.com>
2026-07-11 04:21:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True) # Cria a pasta automaticamente se não existir
 
 db = SQLAlchemy(app)
+with app.app_context():
+        db.create_all()
 
 
 login_manager = LoginManager(app)
